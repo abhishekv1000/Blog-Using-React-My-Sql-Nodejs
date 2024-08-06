bbd06d2965b6b7fed18d8edb831e0b4698ce2abe
abhishekv1000 <abhishekv1000@outlook.com>
2024-08-06 15:48:48 +0530

    DONE

---
+++ db.js
@@ -0,0 +1,8 @@
+import mysql from "mysql"
+
+export const db = mysql.createConnection({
+  host:"bheuebaplw6vledefdjf-mysql.services.clever-cloud.com",
+  user:"u8chf7ncp6aowkhr",
+  password:"NOTzH1vT5d35Zw4TB8CD",
+  database:"bheuebaplw6vledefdjf"
+})
